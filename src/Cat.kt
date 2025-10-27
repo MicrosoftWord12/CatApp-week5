78c1ad9118e038215e2b941fbf627506ac5bbdd0
Microsft-Wrd <mrjacobbrooks@gmail.com>
2025-10-27 13:18:03 +0000

    feat: Added parametter to Cat walking and created while loop for continuous operations

--- src/Cat.kt
+++ src/Cat.kt
@@ -3,10 +3,14 @@ class Cat (val name: String, var age: Int, var weight: Int) {
     // Reduces weight by one
     // Should check if the weight is 5 or more
     // There is a bug - don't resolve until instructed by the exercise.
-    fun walk() :Boolean {
-        if(weight <= 5) {
-            weight--
-            return true
+    fun walk(distance: Int) :Boolean {
+        var currentDistance = distance
+        while(currentDistance >= 0) {
+            if(weight <= 5) {
+                weight--
+                currentDistance--
+                return true
+            }
         }
         return false
     }
--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,8 @@
 fun main() {
     val binnie = Cat("Binnie", 10, 10)
     val clyde = Cat("Clyde", 5, 5)
-    binnie.walk()
-    clyde.walk()
+    binnie.walk(5)
+    clyde.walk(5)
     println(binnie)
     println(clyde)
 }
